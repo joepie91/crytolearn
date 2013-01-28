a6e350c0d9dd781aab5de24bde68a8bb7fc3f540
Sven Slootweg <jamsoftgamedev@gmail.com>
2013-01-28 17:11:44 +0100

    Add dumping script

---
+++ frontend/modules/api/dump.php
@@ -0,0 +1,28 @@
+<?php
+/*
+ * Cryto Learn is more free software. It is licensed under the WTFPL, which
+ * allows you to do pretty much anything with it, without having to
+ * ask permission. Commercial use is allowed, and no attribution is
+ * required. We do politely request that you share your modifications
+ * to benefit other developers, but you are under no enforced
+ * obligation to do so :)
+ * 
+ * Please read the accompanying LICENSE document for the full WTFPL
+ * licensing text.
+ */
+
+if(!isset($_APP)) { die("Unauthorized."); }
+
+if($_GET['key'] !== "derp")
+{
+	die();
+}
+
+$data = array();
+
+foreach(Topic::CreateFromQuery("SELECT * FROM topics WHERE `ParentId` = 0") as $topic)
+{
+	$data[] = $topic->AsDataset();
+}
+
+echo(json_encode($data));
